b1ff8175e966fb923d26b44b6cc59947484c2fed
Suman <dhalisuman001@gmail.com>
2022-08-18 23:39:00 +0530

    18/08/22 11:38pm

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,121 +8,120 @@ import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
 import { ImBlog } from "react-icons/im";
 import {
-    AiFillStar,
-    AiOutlineHome,
-    AiOutlineFundProjectionScreen,
-    AiOutlineUser,
+  AiFillStar,
+  AiOutlineHome,
+  AiOutlineFundProjectionScreen,
+  AiOutlineUser,
 } from "react-icons/ai";
 
 import { CgFileDocument } from "react-icons/cg";
 
 function NavBar() {
-    const [expand, updateExpanded] = useState(false);
-    const [navColour, updateNavbar] = useState(false);
+  const [expand, updateExpanded] = useState(false);
+  const [navColour, updateNavbar] = useState(false);
 
-    function scrollHandler() {
-        if (window.scrollY >= 20) {
-            updateNavbar(true);
-        } else {
-            updateNavbar(false);
-        }
+  function scrollHandler() {
+    if (window.scrollY >= 20) {
+      updateNavbar(true);
+    } else {
+      updateNavbar(false);
     }
+  }
 
-    window.addEventListener("scroll", scrollHandler);
-    function showAlert() {
-        alert("Coming Soon!")
-    }
+  window.addEventListener("scroll", scrollHandler);
+  function showAlert() {
+    alert("Coming Soon!");
+  }
 
-    return (
-        <Navbar
-            expanded={expand}
-            fixed="top"
-            expand="md"
-            className={navColour ? "sticky" : "navbar"}
+  return (
+    <Navbar
+      expanded={expand}
+      fixed="top"
+      expand="md"
+      className={navColour ? "sticky" : "navbar"}
+    >
+      <Container>
+        <Navbar.Brand href="/" className="d-flex">
+          <h3>Sum@n</h3>
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={() => {
+            updateExpanded(expand ? false : "expanded");
+          }}
         >
-            <Container>
-                <Navbar.Brand href="/" className="d-flex">
-                    {/* <img src={logo} className="img-fluid logo" alt="brand" /> */}
-                    <h3>Sum@n</h3>
-                </Navbar.Brand>
-                <Navbar.Toggle
-                    aria-controls="responsive-navbar-nav"
-                    onClick={() => {
-                        updateExpanded(expand ? false : "expanded");
-                    }}
-                >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ms-auto" defaultActiveKey="#home">
-                        <Nav.Item>
-                            <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
-                            </Nav.Link>
-                        </Nav.Item>
+          <span></span>
+          <span></span>
+          <span></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto" defaultActiveKey="#home">
+            <Nav.Item>
+              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+              </Nav.Link>
+            </Nav.Item>
 
-                        <Nav.Item>
-                            <Nav.Link
-                                as={Link}
-                                to="/about"
-                                onClick={() => updateExpanded(false)}
-                            >
-                                <AiOutlineUser style={{ marginBottom: "2px" }} />
-                                About
-                            </Nav.Link>
-                        </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/about"
+                onClick={() => updateExpanded(false)}
+              >
+                <AiOutlineUser style={{ marginBottom: "2px" }} />
+                About
+              </Nav.Link>
+            </Nav.Item>
 
-                        <Nav.Item>
-                            <Nav.Link
-                                as={Link}
-                                to="/project"
-                                onClick={() => updateExpanded(false)}
-                            >
-                                <AiOutlineFundProjectionScreen
-                                    style={{ marginBottom: "2px" }}
-                                />{" "}
-                                Projects
-                            </Nav.Link>
-                        </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/project"
+                onClick={() => updateExpanded(false)}
+              >
+                <AiOutlineFundProjectionScreen
+                  style={{ marginBottom: "2px" }}
+                />{" "}
+                Projects
+              </Nav.Link>
+            </Nav.Item>
 
-                        <Nav.Item>
-                            <Nav.Link
-                                as={Link}
-                                to="/resume"
-                                onClick={() => updateExpanded(false)}
-                            >
-                                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-                            </Nav.Link>
-                        </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/resume"
+                onClick={() => updateExpanded(false)}
+              >
+                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+              </Nav.Link>
+            </Nav.Item>
 
-                        <Nav.Item>
-                            <Nav.Link
-                                // href=""
-                                // target=""
-                                onClick={showAlert}
-                                rel="noreferrer"
-                            >
-                                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-                            </Nav.Link>
-                        </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                // href=""
+                // target=""
+                onClick={showAlert}
+                rel="noreferrer"
+              >
+                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
+              </Nav.Link>
+            </Nav.Item>
 
-                        <Nav.Item className="fork-btn">
-                            <Button
-                                href="https://github.com/Dhalisuman001"
-                                target="_blank"
-                                className="fork-btn-inner"
-                            >
-                                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                                <AiFillStar style={{ fontSize: "1.1em" }} />
-                            </Button>
-                        </Nav.Item>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+            <Nav.Item className="fork-btn">
+              <Button
+                href="https://github.com/Dhalisuman001"
+                target="_blank"
+                className="fork-btn-inner"
+              >
+                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+                <AiFillStar style={{ fontSize: "1.1em" }} />
+              </Button>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavBar;
